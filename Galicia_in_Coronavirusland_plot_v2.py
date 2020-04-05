531217e5caad321c20f25865b546bf94f41ee2b2
mirix <miromoman@gmail.com>
2020-04-05 13:40:15 +0200

    Add files via upload

--- Galicia_in_Coronavirusland_plot_v2.py
+++ Galicia_in_Coronavirusland_plot_v2.py
@@ -140,7 +140,7 @@ plt.close()
 
 ax = plt.gca()
 ax.set_title('COVID-19 pandemic')
-ax.set_ylabel('Absolute deaths per million')
+ax.set_ylabel('Daily deaths per million')
 
 #removing top and right borders
 ax.spines['top'].set_visible(False)
@@ -171,7 +171,7 @@ resultnc.drop(resultnc.head(10).index,inplace=True)
 
 ax = plt.gca()
 ax.set_title('COVID-19 pandemic')
-ax.set_ylabel('Absolute deaths per million')
+ax.set_ylabel('Daily deaths per million')
 
 #removing top and right borders
 ax.spines['top'].set_visible(False)
